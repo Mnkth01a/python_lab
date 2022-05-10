ff2407620a522326f5620d289966ff3280f58546
Tom Hughes <twhughes91159@gmail.com>
2022-05-09 22:17:51 -0400

    initialize repo for python lab

---
+++ new-python-code.py
@@ -0,0 +1,19 @@
+# my first python code
+
+# i get how simple it is, and i should be more advanced than this, but this is for learning the syntax, building experience using vs code, and making sure i understand how the output performs.
+
+print("Hello Cruel World!\n"); # this causes an additional line between this print and the next. print() prints on a new line by default?  Or is something else happening here?
+
+print("I'm still here!");
+
+# let's test it. this printed on a new line.
+print("Well, whadda you know.!?");
+
+# What about this?
+print("Will it be on a new line?"); print("Here's your answer.");
+
+# let's try concatenation. (i spell better than i pronounce.) works as advertised!
+print("Here's part one: " + "and here's part two.");
+
+# So, these are good examples of how simple print() commands work in python.
+
